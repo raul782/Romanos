05772f19adbd3410c580d64220a50ffb42c1e60a
Raul Rodriguez <raulrodriguez782@gmail.com>
2011-11-22 04:06:07 -0500

    Beta Version Roman numbers

--- src/RomanConverter.php
+++ src/RomanConverter.php
@@ -1,38 +1,61 @@
 <?php 
+namespace Romanos;
+
+require_once 'RomanSymbol.php';
+
+use Romanos\RomanSymbol;
+
 class RomanConverter
 {
   public function numberToRoman($number)
  {
-
-	if($number<=3){
-		$romano = "";
-		return $this->repeatLetters("I",$number);
+    $digits = $this->decomposeNumberInReverse($number);
+    $romanInReverse = array();
+    foreach($digits as $position => $digit)
+    {
+        $romanInReverse [] = $this->transformDigitToRoman($digit, $position);
+    }
+    return $this->transformIntoRomanWord($romanInReverse);
 		
-	}
-    if($number == 4){
-      return "IV";
+  }
+
+  public function transformDigitToRoman($digit, $position)
+  {
+       $romanSymbol = new RomanSymbol();
+       $remaining = $digit % 5;
+       $digitToRoman = ""; 
+       if($remaining > 0 && $remaining <= 3) 
+       {
+         $digitToRoman = ($digit>5) ? $romanSymbol->getRomanSymbol(5*pow(10,$position)) :"";
+         $digitToRoman .= $this->repeatLetters($romanSymbol->getRomanSymbol(pow(10,$position)),$remaining);
+       }
+       elseif ($remaining == 4 )
+       {
+          $higherNumber = ($digit <5)?5:10;  
+          $digitToRoman = $romanSymbol->getRomanSymbol(pow(10,$position)) . $romanSymbol->getRomanSymbol($higherNumber*pow(10,$position));
+       }
+	   elseif($remaining == 0 && $digit == 5){
+        $digitToRoman = $romanSymbol->getRomanSymbol(5*pow(10,$position));
+       }
+       return $digitToRoman;
+
     }
-
-	if($number == 5){
-      return "V";
+	
+    public function decomposeNumberInReverse($number)
+    {
+         return array_reverse(str_split($number));
     }
-
-	if( $number < 9 && $number >= 6){
-		$romano = "V";
-		$diff = $number - 5;
-		$romano .= $this->repeatLetters("I", $diff);
-		return $romano;
- 	}
-	
-}
-	
-
 
 	public function repeatLetters($letter, $times)
 	{
 		
 		return str_repeat($letter, $times);
 	}
+
+    public function transformIntoRomanWord($romanInReverse)
+    {
+        return implode("",array_reverse($romanInReverse)); 
+    }
 
 }
 ?>
