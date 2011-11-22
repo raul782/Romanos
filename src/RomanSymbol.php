9013eb2a3347990320533d804fc937281399a4b8
Raul Rodriguez <raulrodriguez782@gmail.com>
2011-11-22 13:48:27 -0500

    Indentantion fix

--- src/RomanSymbol.php
+++ src/RomanSymbol.php
@@ -3,15 +3,17 @@ namespace Romanos;
 
 class RomanSymbol
 {
-	private $symbols =  array ( 1 => "I",
-	 5 => "V",
-	 10 => "X",
-	 50 => "L",
-	 100 => "C",
-	 500 => "D",
-	 1000 => "M");
-	
-		
+	private $symbols =  array ( 
+     1 => "I",
+     5 => "V",
+     10 => "X",
+     50 => "L",
+     100 => "C",
+     500 => "D",
+     1000 => "M"
+    );
+
+
 	public function getRomanSymbol($referenceNumber)
 	{
         if(!in_array($referenceNumber, array_keys($this->symbols)) )
